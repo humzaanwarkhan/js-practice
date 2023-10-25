91c4e49003b9064bcb41b4d9a586a861ba8e30f0
Humza Anwar Khan <114299905+humzaanwarkhan@users.noreply.github.com>
2023-10-25 12:16:22 +0000

    done

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,8 @@
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + " " + str2
+console.log(str3)
+
+console.log("1" + 2 + 3);
+console.log(1 + "2" + "3");
+console.log(1 + 2 + "3");
---
+++ 01_basics/04_strings.js
@@ -0,0 +1,12 @@
+let name = "humza"
+let age = 20
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const collegeName = new String('Integral University')
+console.log(collegeName)
+console.log(collegeName.length)
+
+const newName = "Green view public school"
+console.log(newName.slice(0,7))
+console.log(newName.substring(0,10));
