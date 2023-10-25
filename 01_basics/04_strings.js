87b5e26404ca1c72fba5e022b718b261b8d62911
Humza Anwar Khan <114299905+humzaanwarkhan@users.noreply.github.com>
2023-10-25 17:05:10 +0000

    done

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -1,12 +1,15 @@
-let name = "humza"
-let age = 20
+// let name = "humza"
+// let age = 20
 
-console.log(`Hello my name is ${name} and my age is ${age}`);
+// console.log(`Hello my name is ${name} and my age is ${age}`);
 
-const collegeName = new String('Integral University')
-console.log(collegeName)
-console.log(collegeName.length)
+// const collegeName = new String('Integral University')
+// console.log(collegeName)
+// console.log(collegeName.length)
 
-const newName = "Green view public school"
-console.log(newName.slice(0,7))
-console.log(newName.substring(0,10));
+// const newName = "Green view public school"
+// console.log(newName.slice(0,7))
+// console.log(newName.substring(0,10));
+max = 6
+min = 1
+console.log(Math.floor(Math.random()*(max-min+1))+min)
---
+++ 01_basics/05_date.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+let createdDate = new Date(2023, 9, 11, 5, 6, 8)
+console.log(createdDate.toLocaleString());
